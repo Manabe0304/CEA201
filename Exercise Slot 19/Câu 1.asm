7468accac6a100d44129cce55ebbf04a91ad7c5c
Manabe0304 <hainamn833@gmail.com>
2024-07-16 23:26:27 +0700

    Create Câu 1.asm

---
+++ Exercise Slot 19/Câu 1.asm
@@ -0,0 +1,8 @@
+    include \masm32\include\masm32rt.inc
+
+    .code                       
+
+    start:                         
+        print chr$("Xin chao cac ban!",13,10) ; 13: carriage return, 10: new line
+    exit                            
+    end start                       
