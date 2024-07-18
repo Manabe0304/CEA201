c0af406359bcb4e2ab67705e4744fe8e9d89a1a1
Manabe0304 <hainamn833@gmail.com>
2024-07-18 10:22:44 +0700

    Create bai12.asm

---
+++ Exercise Slot 19/bai12.asm
@@ -0,0 +1,32 @@
+include  \masm32\include\masm32rt.inc
+
+sum PROTO:DWORD, :DWORD
+
+.code
+
+vidu:
+    call ham1                                 ;goi ham khong truyen gia tri 
+    exit
+
+    ham1 proc
+        LOCAL a: DWORD                        ;khai bao bien cuc bo 
+        LOCAL b: DWORD
+        LOCAL kq: DWORD
+        MOV a, sval(input("Nhap a: "))        ;SVAL chuyen string sang number
+        MOV b, sval(input("Nhap b: "))
+
+        INVOKE sum,a,b                        ;INVOKE goi ham co truyen bien
+
+        mov kq, eax
+        print chr$("a / b = ")
+        print str$(kq)
+        ret
+    ham1 endp 
+
+    sum proc x:DWORD, y:DWORD
+        mov eax, x
+        xor edx, edx                              ; Clear EDX before division
+        div y                                     ; Unsigned division
+        ret
+    sum endp   
+end vidu
