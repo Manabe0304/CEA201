76fc904906866513ef8695e21ccefdbf5b3cfd47
Manabe0304 <hainamn833@gmail.com>
2024-07-18 09:45:40 +0700

    Create Cau_4.asm

---
+++ Exercise Slot 19/Cau_4.asm
@@ -0,0 +1,25 @@
+include  \masm32\include\masm32rt.inc
+    
+bienso PROTO :DWORD
+.code
+
+start:
+    call main
+    exit
+
+main proc
+    LOCAL so: DWORD  
+    LOCAL kq: DWORD
+    MOV so, sval(input(13))   
+    invoke bienso, so 
+
+    MOV kq, eax
+    print str$(kq)
+    ret
+main endp
+
+bienso proc string:DWORD                   
+    ret
+bienso endp
+
+end start      
