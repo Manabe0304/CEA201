2462cc56d2507f59c6f0c046d6b2642e54dbbcf2
Manabe0304 <hainamn833@gmail.com>
2024-07-20 19:00:48 +0700

    Update bai5.asm

--- Exercise Slot 19/bai5.asm
+++ Exercise Slot 19/bai5.asm
@@ -5,10 +5,10 @@ sum PROTO:DWORD, :DWORD
 .code
 
 start:
-    call ham1                                 ;goi ham khong truyen gia tri 
+    call main                                 ;goi ham khong truyen gia tri 
     exit
 
-    ham1 proc
+    main proc
         LOCAL a: DWORD                        ;khai bao bien cuc bo 
         LOCAL b: DWORD
         LOCAL kq: DWORD
@@ -21,7 +21,7 @@ start:
         print chr$("a + b = ")
         print str$(kq)
         ret
-    ham1 endp 
+    main endp 
 
     sum proc x:DWORD, y:DWORD
         mov eax, x
